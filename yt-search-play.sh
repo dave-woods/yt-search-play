5a30da809d87966185af7f7f937e3adff8458775
Dave Woods <dwoodscs@gmail.com>
2021-01-18 22:18:02 +0000

    Implement caching -- slightly buggy?

--- yt-search-play.sh
+++ yt-search-play.sh
@@ -2,19 +2,20 @@
 
 ## Initialise variables
 
-# configfile="$(dirname $0)/config"
-# if [[ -f "$config" ]]
-# then
 search_N=30
 max_history_size=50
-# fi
+max_cache_age=60
+force_no_cache=
+
 data_path="$(dirname $0)/.data"
+mkdir -p "$data_path"
+
 historyfile="$data_path/search-history"
 cookiefile="$data_path/cookies.txt"
+cachefile="$data_path/cache.json"
 
 if [[ ! -f "$historyfile" ]]
 then
-	mkdir -p "$data_path"
 	touch -a "$historyfile"
 fi
 history_size=$(< "$historyfile" wc -l 2>/dev/null || echo 0)
@@ -39,25 +40,69 @@ update_history () {
 	fi
 }
 
+write_to_cache () {
+	if [[ -n "$2" ]]
+	then
+		if [[ ! -s "$cachefile" ]] || [[ -z "$(cat "$cachefile" | tr -d '[:space:]')" ]]
+  	then
+  	  echo "[]" > "$cachefile"
+  	fi
+
+  	tail --pid="$2" -f /dev/null
+  	local cache=$(<"$temp")
+  
+		jq --arg sel "$1" 'del(.[] | select(.entry == $sel))' "$cachefile" | jq --arg sel "$1" --arg time "$(date +%s)" --arg cc "$cache" '. += [{entry: $sel, time: $time, cache: $cc}]' > "$cachefile.tmp" && mv "$cachefile.tmp" "$cachefile"
+  
+	fi
+	rm -f "$temp"
+}
+
+read_from_cache () {
+	if [[ -f "$cachefile" ]]
+  then
+    local entry_found=$(jq --arg sel "$1" '.[] | select(.entry == $sel)' "$cachefile")
+    if [[ -n "$entry_found" ]] && [[ $(($(date +%s)-$(echo "$entry_found" | jq -r '.time'))) -lt $max_cache_age ]]
+    then
+      echo "$entry_found"
+    fi
+  fi 
+}
+
 find_video () {
-	local temp=$(mktemp /tmp/ysp_idx.XXXXXXXX)
 	local search_url="$1"
 	selection=
 	url=
 	
-	youtube-dl -i --playlist-end $search_N $include_cookies -j "$search_url" \
-		| jq --unbuffered -r '. | "\(.fulltitle) :: \(.uploader) => \(.webpage_url)"' > "$temp" &
-	local idx=$(tail -f "$temp" | stdbuf -o0 awk 'BEGIN{FS=OFS=" => "}{NF--; print}' \
-		| rofi -dmenu -i -p 'Select Video' -no-show-icons -l 10 -scroll-method 0 -format i -async-pre-read 0 &)
+	[[ -z "$force_no_cache" ]] && local cache_result=$(read_from_cache "$search_url")
 	
-	pkill youtube-dl # found result, stop searching
-	
-	if [[ -n "$idx" ]]
+	if [[ -z "$cache_result" ]]
 	then
-		selection=$(sed "$((idx+1))q;d" "$temp")
-		url=$(echo "$selection" | awk 'BEGIN{FS=OFS=" => "}{print $NF}')
+		temp=$(mktemp /tmp/ysp_idx.XXXXXXXX)
+		
+		( youtube-dl -i --playlist-end $search_N $include_cookies -j "$search_url" & echo $! >&3 ) 3>pid \
+			| jq --unbuffered -r '. | "\(.fulltitle) :: \(.uploader) => \(.webpage_url)"' > "$temp" &
+		ytdl_pid=$(<pid)
+
+		idx=$(tail -f "$temp" | stdbuf -o0 awk 'BEGIN{FS=OFS=" => "}{NF--; print}' \
+			| rofi -dmenu -i -p 'Select Video' -no-show-icons -l 10 -scroll-method 0 -format i -async-pre-read 0 &)
+		
+		if [[ -n "$idx" ]]
+		then
+			selection=$(sed "$((idx+1))q;d" "$temp")
+		fi
+
+		[[ -z "$force_no_cache" ]] && write_to_cache "$search_url" $ytdl_pid || (kill $ytdl_pid 2>/dev/null; rm -f "$temp") &
+	else
+		local cache_value=$(echo "$cache_result" | jq -r '.cache')
+    idx=$(echo "$cache_value" | stdbuf -o0 awk 'BEGIN{FS=OFS=" => "}{NF--; print}' \
+      | rofi -dmenu -i -p 'Select Video' -no-show-icons -l 10 -scroll-method 0 -format i -async-pre-read 0 &)
+    if [[ -n "$idx" ]]
+    then
+      selection=$(echo "$cache_value" | sed "$((idx+1))q;d")
+    fi
 	fi
-	rm -f "$temp"
+
+	url=$(echo "$selection" | awk 'BEGIN{FS=OFS=" => "}{print $NF}')
 }
 
 process_args () {
